b22a33634bcec21c3c6943c47c5449a911a487d2
Iasmincost134 <133365214+Iasmincost134@users.noreply.github.com>
2023-09-10 02:18:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-
-let imgIndice = 0;
-let IMAGENS = ["img/image1.jpg", "img/image2.png", "img/imagem3.png", "img/imagem4.png", "img/imagem5.png"];
-const IMG = document.getElementById('carrosselImagem');
-const totalDeQuestoes = 7;
-const limiteTentativas = 2;
 let pontos = 0;
-let placar = 0;
-let tentativas = 0;
+let vidasRestantes = 2;
+//carrossel de imagens
+const IMAGENS = ["img/image1.jpg", "img/aaa.JPG", "img/adam.JPG", "img/mitocondria.JPG", "img/grito.JPG", "img/Torre.jpg", "img/Jupiter.jpg"];
+const IMG = document.getElementById('carrosselImagem');
+let imgIndice = 0;
 
+//Constante relacionadas às questões
+const totalDeQuestoes = 7;
 const questoes = [
   {
     pergunta: "Quem foi o principal líder do Anarquismo?",
     alternativas: ["Karl Marx", "John Locke", "Mikhail Bakunin", "Thomas Hobbes"],
     correta: "Mikhail Bakunin"
+    
   },
   {
     pergunta: "Qual alternativa exemplifica um processo Endotérmico?",
@@ -46,62 +46,124 @@ const questoes = [
   }
 ];
 
-let numQuestao = document.querySelector('#numQuestao');
-let pergunta = document.querySelector('#pergunta');
-let alternativas = document.querySelector('#alternativas');
-// let alternativas = document.querySelector('#alternativas');
-let instrucoes = document.querySelector('#instrucoes');
-
+//elementos HTML
+let numeroQuestao = document.querySelector('#numQuestao');
+let perguntaElemento = document.querySelector('#pergunta');
+let alternativasElemento = document.querySelector('#alternativas');
+let instrucoesElemento = document.querySelector('#instrucoes');
+let tentativasElemento = document.querySelector('#tentativas');
+let elementoPergunta = document.getElementById('pergunta');
 let numero = document.querySelector('#numero');
 let total = document.querySelector('#total');
+let vidasElement = document.getElementById('vidas');
 
 numero.textContent = 1;
-total.textContent = totalDeQuestoes;
+total.textContent = questoes.length;
 
+// Função responsável por carregar a próxima questão
 function carregarProximaQuestao(nQuestao) {
-    numero.textContent = nQuestao + 1;
-    pergunta.textContent = questoes[nQuestao].pergunta;
-    for (let i = 0; i < questoes[nQuestao].alternativas.length; i++) {
-        alternativas.children[i].textContent = questoes[nQuestao].alternativas[i];
-    }
+   // Atualiza o número da questão exibido no HTML
+  numero.textContent = nQuestao + 1;
+   // Carrega a pergunta da próxima questão
+  perguntaElemento.textContent = questoes[nQuestao].pergunta;
+  
+  // Atualiza as alternativas de resposta para a próxima questão
+  for (let i = 0; i < questoes[nQuestao].alternativas.length; i++) {
+    alternativasElemento.children[i].textContent = questoes[nQuestao].alternativas[i];
+  }
 }
-for (let i = 0; i < alternativas.children.length; i++) {
-    alternativas.children[i].addEventListener('click', function() {
-        verificarResposta(numero.textContent - 1, alternativas.children[i].textContent);
-    });
-}
-//faltava isso para ir a proxima alternatica(não esquecer) 
 
+// Função para verificar a resposta escolhida pelo usuário
 function verificarResposta(nQuestao, respostaEscolhida) {
-    let correta = questoes[nQuestao].correta;
-    if (respostaEscolhida === correta) {
-        pontos += 10;
-        imgIndice += 1;
-    } else {
-        imgIndice = 0;
-    }
-    placar = pontos;
-    instrucoes.textContent = "Pontos " + placar;
+  let correta = questoes[nQuestao].correta;
+  let feedbackElemento = document.getElementById('feedback');
 
-    if (nQuestao + 1 >= totalDeQuestoes) {
-        fimDoJogo();
+  if (respostaEscolhida === correta) {
+    pontos+=10;
+    feedbackElemento.textContent = "Resposta correta!";
+    feedbackElemento.style.color = "green";
+  } else {
+    vidasRestantes--;
+    atualizarVidas();
+    feedbackElemento.textContent = "Resposta incorreta!";
+    feedbackElemento.style.color = "red";
+    if (vidasRestantes > 0) {
+       // Caso ainda existam vidas restantes
+      instrucoes.textContent = "Você perdeu, mas ainda tem " + vidasRestantes + " vida(s) restante(s). Reiniciando a próxima pergunta...";
+      carregarProximaQuestao(0);
     } else {
-        setTimeout(function() {
-            carregarProximaQuestao(nQuestao + 1); 
-        }, 1000);
+      // Caso tenha perdido todas as vidas
+      instrucoes.textContent = "Fim de Jogo!";
+      pergunta.textContent = "Você conseguiu " + pontos + (pontos === 1 ? " ponto" : " pontos");
+      pontos = 0;
+      articleQuestoes.style.display = 'none';
     }
+  }
+
+  imgIndice += 1;
+  carregar();
+  placar = pontos;
+  instrucoes.textContent = "Pontos: " + placar;
+ // Verifica se todas as questões foram respondidas
+  if (nQuestao + 1 >= questoes.length) {
+    fimDoJogo();
+  } else {
+    setTimeout(function () {
+      carregarProximaQuestao(nQuestao + 1); // Carrega a próxima questão após um intervalo de 1 segundo
+    }, 1000);
+  }
 }
+function carregar() {
+  IMG.src = IMAGENS[imgIndice];
+}
+// Função que é chamada quando o jogo acaba
 function fimDoJogo() {
+  if (vidasRestantes === 0) {
+     // Caso o jogador perdeu todas as vidas
     instrucoes.textContent = "Fim de Jogo!";
     pergunta.textContent = "Você conseguiu " + pontos + (pontos === 1 ? " ponto" : " pontos");
     pontos = 0;
     articleQuestoes.style.display = 'none';
+  } else {
+    //Caso o jogador ganhou mas ainda tem vidas restantes
+    carregarProximaQuestao(0); 
+    instrucoes.textContent = "Você ganhou, mas ainda tem " + vidasRestantes + " vida(s) restante(s). Reiniciando a próxima pergunta...";
+  }
 }
 
-function carregar() {
-    IMG.src = IMAGENS[imgIndice];
+function reiniciarJogo() {
+  imgIndice = 0;
+  pontos = 0;
+  vidasRestantes = 2;
+  instrucoes.textContent = "Bem-vindo de volta ao início do Quiz!";
+  atualizarVidas();
+  carregarProximaQuestao(0);
+  carregar();
+  elementoPergunta.style.display = 'block'; // Exibe o elemento de pergunta no HTML
+  tentativasElemento.style.display = 'block';
+  feedbackElemento.textContent = '';
+  feedbackElemento.classList.remove('feedback-correta', 'feedback-incorreta');
+  //Remove a classe de seleção nas alternativas de resposta
+  for (let i = 0; i < alternativasElemento.children.length; i++) {
+    alternativasElemento.children[i].classList.remove('opcao-selecionada');
+  }
 }
 
-// Chame as funções iniciais
-carregar();
-carregarProximaQuestao(0);
+function atualizarVidas() {
+  vidasElement.innerHTML = 'Vidas: ';
+
+  for (let i = 0; i < vidasRestantes; i++) {
+    const vidaElement = document.createElement('span');
+    vidaElement.className = 'vida';
+    vidaElement.textContent = '❤️';
+    vidasElement.appendChild(vidaElement);
+  }
+
+  for (let i = 0; i < 3 - vidasRestantes; i++) {
+    const vidaVaziaElement = document.createElement('span');
+    vidaVaziaElement.className = 'vida-vazia';
+    vidaVaziaElement.textContent = '🖤';
+    vidasElement.appendChild(vidaVaziaElement);
+  }
+}
+reiniciarJogo()
